fb25a5334d49b4ab1bdfdae8df6fb1aeb6a54034
Mayank <mayank2157@gmail.com>
2022-08-31 19:23:15 +0530

    make it better

--- mern-chat-app/frontend/src/index.js
+++ mern-chat-app/frontend/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { ChakraProvider } from "@chakra-ui/react";
 import ChatProvider from "./Context/ChatProvider";
 import { BrowserRouter } from "react-router-dom";
